f3ce5cba2904bb5adb5041e681bd111a1b0800c9
Reagan <kingreggie702@gmail.com>
2022-09-03 11:22:29 +0200

    Changes

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ router.patch('/users', bodyparser.json(), (req,res)=>{
                             if(auth){
                                 const payload = {
                                     user:{
-                                        id: results[0].id,
+                                        userId: results[0].userId,
                                         firstName: results[0].firstName,
                                         lastName: results[0].lastName,
                                         userPassword: results[0].userPassword,
